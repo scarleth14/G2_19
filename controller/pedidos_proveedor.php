acf77ec8359a1e7a8fef592df67d7ac4d8a65ce3
AnthonyAvila11 <ajavilal@unah.hn>
2022-03-19 13:12:16 -0600

    ma_pedidos_proveedor completo

---
+++ controller/pedidos_proveedor.php
@@ -0,0 +1,47 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] === 'OPTIONS'){
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+    }
+        header('Access-Control-Allow-Origin: *');
+        header('Content-Type: application/json');
+
+        require_once("../config/conexion.php");
+        require_once("../models/Pedidos_Proveedor.php");
+        $pedidos = new Pedidos();
+
+        $body = json_decode(file_get_contents("php://input"), true);
+
+        switch($_GET["op"]){
+
+            case "GetPedidos":
+                $datos=$pedidos->get_pedidos();
+                echo json_encode($datos);
+            break;    
+            
+            case "GetPedido":
+                $datos=$pedidos->get_pedido($body["ID"]);
+                echo json_encode($datos);
+            break;
+
+            case "InsertPedido":
+                $datos=$pedidos->insert_pedido($body["ID_SOCIO"], $body["FECHA_PEDIDO"], $body["DETALLE"], $body["SUB_TOTAL"], $body["TOTAL_ISV"], $body["TOTAL"], $body["FECHA_ENTREGA"], $body["ESTADO"]);
+                echo json_encode("Pedido de Proveedor Agregado");
+            break;
+
+            case "UpdatePedido":
+                $datos=$pedidos->update_pedido($body["ID"], $body["FECHA_PEDIDO"], $body["SUB_TOTAL"], $body["TOTAL_ISV"], $body["TOTAL"], $body["FECHA_ENTREGA"], $body["ESTADO"]);
+                echo json_encode("Pedido de Proveedor Actualizado");
+            break;
+
+            case "DeletePedido":
+                $datos=$pedidos->delete_pedido($body["ID"]);
+                echo json_encode("Pedido de Proveedor Eliminado");
+            break;
+        }
+?>
---
+++ models/Pedidos_Proveedor.php
@@ -0,0 +1,71 @@
+<?php
+
+    class Pedidos extends Conectar{
+
+        public function get_pedidos(){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM g2_19.ma_pedidos_proveedor ";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }        
+        
+        public function get_pedido($ID){
+            $conectar=parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM g2_19.ma_pedidos_proveedor WHERE ID = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $ID);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function insert_pedido($ID_SOCIO, $FECHA_PEDIDO, $DETALLE, $SUB_TOTAL, $TOTAL_ISV, $TOTAL, $FECHA_ENTREGA, $ESTADO){
+            $conectar=parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO g2_19.ma_pedidos_proveedor(ID_SOCIO, FECHA_PEDIDO, DETALLE, SUB_TOTAL, TOTAL_ISV, TOTAL, FECHA_ENTREGA, ESTADO)
+            VALUES (?,?,?,?,?,?,?,?);";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $ID_SOCIO);
+            $sql->bindValue(2, $FECHA_PEDIDO);
+            $sql->bindValue(3, $DETALLE);
+            $sql->bindValue(4, $SUB_TOTAL);
+            $sql->bindValue(5, $TOTAL_ISV);
+            $sql->bindValue(6, $TOTAL);
+            $sql->bindValue(7, $FECHA_ENTREGA);
+            $sql->bindValue(8, $ESTADO);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function update_pedido($ID, $FECHA_PEDIDO, $SUB_TOTAL, $TOTAL_ISV, $TOTAL, $FECHA_ENTREGA, $ESTADO){
+            $conectar=parent::conexion();
+            parent::set_names();
+            $sql="UPDATE g2_19.ma_pedidos_proveedor SET FECHA_PEDIDO = ?, SUB_TOTAL = ?, TOTAL_ISV = ?, TOTAL = ?, FECHA_ENTREGA = ?, ESTADO = ? WHERE ID = ?;";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $FECHA_PEDIDO);
+            $sql->bindValue(2, $SUB_TOTAL);
+            $sql->bindValue(3, $TOTAL_ISV);
+            $sql->bindValue(4, $TOTAL);
+            $sql->bindValue(5, $FECHA_ENTREGA);
+            $sql->bindValue(6, $ESTADO);
+            $sql->bindValue(7, $ID);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function delete_pedido($ID){
+            $conectar=parent::conexion();
+            parent::set_names();
+            $sql="DELETE FROM g2_19.ma_pedidos_proveedor WHERE ID = ?;";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $ID);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+    
+
+
+?>
