a5b1d2dbfa08e6ebc65c773f59ba3d4637daf88a
VARCHAR0Rafael <rtorresc@unah.hn>
2022-03-19 13:39:15 -0600

    factura cliente agregada

---
+++ controller/factura_cliente.php
@@ -0,0 +1,51 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] === '0PTIONS'){
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Method: POST, GET, DELETE, PUT, PATHC, OPTION');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    require_once("../config/conexion.php");
+    require_once("../models/Factura_Cliente.php");
+    $factura = new Factura();
+
+    $body = json_decode(file_get_contents("php://input"), true);
+
+    switch($_GET["op"]){
+
+        case "GetFacturas":
+            $datos=$factura->get_facturas();
+            echo json_encode($datos);
+        break;
+
+        case "GetFactura":
+            $datos=$factura->get_factura($body["ID"]);
+            echo json_encode($datos);
+        break;
+
+        case "InsertFactura":
+            $datos=$factura->insert_factura($body["NUMERO_FACTURA"],$body["ID_SOCIO"],$body["FECHA_FACTURA"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_VENCIMIENTO"],$body["ESTADO"],);
+            echo json_encode("Factura del cliente Agregada");
+        break;
+
+        case "UpdateFactura":
+            $datos=$factura->update_factura($body["ID"],$body["NUMERO_FACTURA"],$body["ID_SOCIO"],$body["FECHA_FACTURA"],$body["DETALLE"],$body["SUB_TOTAL"],$body["TOTAL_ISV"],$body["TOTAL"],$body["FECHA_VENCIMIENTO"],$body["ESTADO"],);
+            echo json_encode("Factura del cliente Actualizado");
+        break;
+
+        case "DeleteFactura":
+            $datos=$factura->delete_factura($body["ID"]);
+            echo json_encode("Factura del cliente Eliminada");
+        break;
+    }
+
+
+
+
+?>
---
+++ models/Factura_Cliente.php
@@ -0,0 +1,78 @@
+<?php
+
+    class Factura extends Conectar{
+
+        public function get_facturas(){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM ma_factura_cliente ";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function get_factura($id){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM ma_factura_cliente WHERE ID = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function insert_factura($numerofactura, $idsocio, $fechafactura, $detalle, $subtotal, $totalisv, $total, $fechavencimiento, $estado){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO ma_factura_cliente(numero_factura,id_socio,fecha_factura,detalle,sub_total,total_isv,total,fecha_vencimiento,estado)
+            VALUES (?,?,?,?,?,?,?,?,?);";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $numerofactura);
+            $sql->bindValue(2, $idsocio);
+            $sql->bindValue(3, $fechafactura);
+            $sql->bindValue(4, $detalle);
+            $sql->bindValue(5, $subtotal);
+            $sql->bindValue(6, $totalisv);
+            $sql->bindValue(7, $total);
+            $sql->bindValue(8, $fechavencimiento);
+            $sql->bindValue(9, $estado);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function update_factura($id, $numerofactura, $idsocio, $fechafactura, $detalle, $subtotal, $totalisv, $total, $fechavencimiento, $estado){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql="UPDATE ma_factura_cliente SET numero_factura=?, id_socio=?, fecha_factura=?, detalle=?, sub_total=?, total_isv=?, total=?, fecha_vencimiento=?, estado=? WHERE ID = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $numerofactura);
+            $sql->bindValue(2, $idsocio);
+            $sql->bindValue(3, $fechafactura);
+            $sql->bindValue(4, $detalle);
+            $sql->bindValue(5, $subtotal);
+            $sql->bindValue(6, $totalisv);
+            $sql->bindValue(7, $total);
+            $sql->bindValue(8, $fechavencimiento);
+            $sql->bindValue(9, $estado);
+            $sql->bindValue(10, $id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function delete_factura($id){
+            $conectar = parent::conexion();
+            parent::set_names();
+            $sql="DELETE FROM ma_factura_cliente WHERE ID = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+    }
+
+    
+
+
+
+?>
